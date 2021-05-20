eaa6027dbf3d1608ad9ded86814f024d405c9a9d
zMed-tech <badache.amn@gmail.com>
2021-05-20 03:25:25 +0100

    feat: add google font

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        additionalData: `@import "@/assets/style/variables.scss";`,
+        additionalData: `@import "@/assets/style/variables.scss";
+        @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Satisfy&display=swap');`,
       },
     },
   },
