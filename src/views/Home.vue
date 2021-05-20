a468154a99f79ba400410a3dc7beab20a806206f
zMed-tech <badache.amn@gmail.com>
2021-05-20 04:01:24 +0100

    feat: organize home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,76 @@
 <template>
   <div id="home">
     <TheNavBar />
+    <TheSlideBar />
     <div class="bg-home"></div>
+    <div class="text">
+      Prenez Votre Rendez-Vous <br />
+
+      <span class="typing">{{ text }}</span>
+    </div>
+    <HomeSvg />
   </div>
 </template>
 
 <script>
 import TheNavBar from "@/components/TheNavBar.vue";
+import TheSlideBar from "@/components/TheSlideBar.vue";
+import HomeSvg from "@/components/HomeSvg.vue";
+
 export default {
   name: "Home",
-  components: { TheNavBar },
+  components: { TheNavBar, TheSlideBar, HomeSvg },
+
+  created() {
+    setInterval(() => {
+      if (this.index == this.words[this.currentWord].length) {
+        this.index = 0;
+        this.text = "";
+        this.currentWord++;
+
+        if (this.currentWord == this.words.length) {
+          this.currentWord = 0;
+        }
+      }
+
+      this.text += this.words[this.currentWord][this.index];
+      this.index++;
+    }, 500);
+  },
+
+  data() {
+    return {
+      words: ["BLS", "VFS", "TLS"],
+      text: "",
+      currentWord: 0,
+      index: 0,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@keyframes tik {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
 #home {
   height: $default-height;
   width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: row;
+
+  & .svg {
+    height: 70%;
+    width: 0%;
+    border: 1px solid red;
+  }
 
   & .bg-home {
     position: absolute;
@@ -25,6 +79,31 @@ export default {
     width: 100%;
     background: linear-gradient(to bottom right, $color-primary, $color-space);
     clip-path: polygon(0% 50%, 100% 10%, 100% 0%, 0% 0%, 0% 50%);
+  }
+
+  & .text {
+    font-family: "Pacifico", cursive;
+
+    color: $color-secondary;
+    text-align: center;
+    font-size: 2rem;
+
+    & .typing {
+      font-family: "Satisfy", cursive;
+      position: relative;
+      font-weight: bold;
+      color: $color-accent;
+
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        height: 100%;
+        width: 1.5px;
+        background-color: black;
+        animation: tik 0.7s ease infinite;
+      }
+    }
   }
 }
 </style>
