cedca32e3c38539b1227093ff5e20cd4e2e39852
zMed-tech <badache.amn@gmail.com>
2021-05-20 20:01:44 +0100

    feat: add formFam and FormInd component

---
+++ src/components/FormFam.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="form-fam"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#form-fam {
+  margin-top: $nav-height;
+  background-color: red;
+}
+</style>
---
+++ src/components/FormInd.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="form-ind"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#form-ind {
+  margin-top: $nav-height;
+  background-color: red;
+}
+</style>
