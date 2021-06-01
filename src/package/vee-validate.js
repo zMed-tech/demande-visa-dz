53fba21611268b5310e0f2483a207cb434b71aa7
zMed-tech <badache.amn@gmail.com>
2021-06-01 20:12:03 +0100

    feat: add alpha_spaces rule

--- src/package/vee-validate.js
+++ src/package/vee-validate.js
@@ -1,7 +1,14 @@
 import { extend } from "vee-validate";
-import { required, alpha, integer, email } from "vee-validate/dist/rules";
+import {
+  required,
+  alpha,
+  integer,
+  email,
+  alpha_spaces,
+} from "vee-validate/dist/rules";
 
 extend("email", email);
 extend("required", required);
 extend("integer", integer);
 extend("alpha", alpha);
+extend("alpha_spaces", alpha_spaces);
