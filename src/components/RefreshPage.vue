366b77444a174c5c3b840e33e2dfd2c181493291
zMed-tech <badache.amn@gmail.com>
2021-09-29 18:33:30 +0100

    feat: create refresh component

---
+++ src/components/RefreshPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="refresh-page">
+    <div class="text">New content is available; please refresh</div>
+    <div class="button">
+      <button>Refresh</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.refresh-page {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: row;
+  padding: 1rem 1rem;
+  border-radius: 5px;
+  background-color: $color-primary;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+
+  & .text {
+    font-size: 1.1rem;
+    font-weight: bold;
+    margin: 0 1rem;
+  }
+
+  & .button {
+    & button {
+      padding: 2px 5px;
+      border-radius: 25px;
+      font-weight: bold;
+      font-size: 1.2rem;
+      cursor: pointer;
+      color: white;
+      background-color: $color-space;
+      transition: 0.7s ease-in;
+
+      &:hover {
+        color: $color-dark;
+      }
+    }
+  }
+}
+</style>
